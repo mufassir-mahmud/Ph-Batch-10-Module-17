785748039456c49fe78069ac0f747c58e5d28728
mufassir-mahmud <mufassirmahmud01821@gmail.com>
2024-08-28 23:10:23 +0600

    first commit

---
+++ types.js
@@ -0,0 +1,4 @@
+var number = 389;
+var name = "Mufassir Mahmud";
+var isSingle = false;
+console.log(typeof number)
---
+++ variable.js
@@ -0,0 +1,7 @@
+var weight = 75;
+var price = 5;
+var year = 2002;
+var money = 2500;
+var age;
+console.log(weight)
+console.log(age);
