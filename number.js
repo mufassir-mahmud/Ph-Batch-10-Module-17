ed53cc2ba21a92f616083650e96147e23dd1730a
mufassir-mahmud <mufassirmahmud01821@gmail.com>
2024-08-29 17:27:57 +0600

    number

---
+++ number.js
@@ -0,0 +1,8 @@
+var orange = 10.5;
+var komola = 10.5  ;
+console.log(orange + komola);
+console.log(typeof komola);
+var num1 = 0.1;
+var num2 = 0.2;
+var total = num1+num2;
+console.log(total.toFixed(2));
